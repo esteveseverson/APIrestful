575fc071a89fe45b6fdf9b73069f6a544a75c9d3
Everson Esteves <esteveseverson@gmail.com>
2024-06-29 15:11:53 -0300

    APIrestful conlcuida

---
+++ go.mod
@@ -0,0 +1,3 @@
+module APIrestful
+
+go 1.22.4
--- main.go
+++ main.go
@@ -1,57 +1,89 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"regexp"
+	"strconv"
 )
 
 type Pessoa struct {
-	ID   string
-	Nome string
+	ID   int    `json:"id"`
+	Nome string `json:"nome"`
 }
+
+var pessoas []Pessoa
+var nextID int = 1
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	switch {
 	case r.Method == "GET" && r.URL.Path == "/pessoa":
-		fmt.Println("Entrou no metodo GET/pessoa")
-		w.Write([]byte("Entrou no metodo GET/pessoa"))
+		getPessoas(w, r)
 	case r.Method == "GET" && regexp.MustCompile(`^/pessoa/\d+$`).MatchString(r.URL.Path):
-		fmt.Println("Entrou no metodo GET/pessoa/{id}")
-		w.Write([]byte("Entrou no metodo GET/pessoa/{id}"))
+		getPessoaByID(w, r)
 	case r.Method == "POST" && r.URL.Path == "/pessoa":
-		fmt.Println("Entrou no metodo POST/pessoa")
-		w.Write([]byte("Entrou no metodo POST/pessoa"))
+		createPessoa(w, r)
 	case r.Method == "DELETE" && regexp.MustCompile(`^/pessoa/\d+$`).MatchString(r.URL.Path):
-		fmt.Println("Entrou no metodo DELETE/pessoa/{id}")
-		w.Write([]byte("Entrou no metodo DELETE/pessoa/{id}"))
+		deletePessoaByID(w, r)
 	default:
-		fmt.Println("Metodo não cadastrado")
-		w.Write([]byte("Metodo não cadastrado"))
+		http.Error(w, "Metodo não cadastrado", http.StatusNotFound)
 	}
 }
 
+func getPessoas(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(pessoas)
+}
+
+func getPessoaByID(w http.ResponseWriter, r *http.Request) {
+	idStr := regexp.MustCompile(`\d+`).FindString(r.URL.Path)
+	id, _ := strconv.Atoi(idStr)
+
+	for _, p := range pessoas {
+		if p.ID == id {
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(p)
+			return
+		}
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(nil)
+}
+
+func createPessoa(w http.ResponseWriter, r *http.Request) {
+	var p Pessoa
+	err := json.NewDecoder(r.Body).Decode(&p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+	p.ID = nextID
+	nextID++
+	pessoas = append(pessoas, p)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(p)
+}
+
+func deletePessoaByID(w http.ResponseWriter, r *http.Request) {
+	idStr := regexp.MustCompile(`\d+`).FindString(r.URL.Path)
+	id, _ := strconv.Atoi(idStr)
+
+	newPessoas := pessoas[:0]
+	for _, p := range pessoas {
+		if p.ID != id {
+			newPessoas = append(newPessoas, p)
+		}
+	}
+	pessoas = newPessoas
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(pessoas)
+}
+
 func main() {
-
-	/*httpObj := NewHttpInterface()
-
-	http.Handle("/foo", httpObj)*/
 
 	http.HandleFunc("/", handler)
 	fmt.Println("Servidor rodando na porta 8080")
 	http.ListenAndServe(":8080", nil)
 
 }
-
-/*type HttpStructImplementation struct {
-}
-
-func NewHttpInterface() http.Handler {
-	return &HttpStructImplementation{}
-}
-
-func (h *HttpStructImplementation) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
-	rw.Header().Set("Content-Type", "application/json")
-	rw.WriteHeader(http.StatusOK)
-	rw.Write([]byte("123"))
-}*/
